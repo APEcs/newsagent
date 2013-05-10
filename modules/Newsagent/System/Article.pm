d626a616b1e3ee5b7a604d566cfdf44cadd8fe6c
Chris <chris@starforge.co.uk>
2013-05-10 15:12:50 +0100

    Image file handling stuff.

--- modules/Newsagent/System/Article.pm
+++ modules/Newsagent/System/Article.pm
@@ -23,11 +23,16 @@ use strict;
 use base qw(Webperl::SystemModule); # This class extends the Newsagent block class
 use v5.12;
 
+use File::Path qw(make_path);
+use File::Copy;
+use Digest;
 use Webperl::Utils qw(path_join);
 
 sub get_user_levels {
     my $self = shift;
     my $user = shift;
+
+    $self -> clear_error();
 
     my $levelsh = $self -> {"dbh"} -> prepare("SELECT * FROM `".$self -> {"settings"} -> {"database"} -> {"levels"}."`
                                                ORDER BY `id`");
@@ -50,6 +55,8 @@ sub get_user_sites {
     my $self = shift;
     my $user = shift;
 
+    $self -> clear_error();
+
     my $sitesh = $self -> {"dbh"} -> prepare("SELECT * FROM `".$self -> {"settings"} -> {"database"} -> {"sites"}."`
                                               ORDER BY `name`");
     $sitesh -> execute()
@@ -70,6 +77,8 @@ sub get_user_sites {
 sub get_file_images {
     my $self = shift;
 
+    $self -> clear_error();
+
     my $imgh = $self -> {"dbh"} -> prepare("SELECT id, name, location
                                             FROM `".$self -> {"settings"} -> {"database"} -> {"images"}."`
                                             WHERE `type` = 'file'
@@ -85,6 +94,253 @@ sub get_file_images {
     }
 
     return \@imagelist;
+}
+
+
+## @method $ get_image_info($id)
+# Obtain the storage information for the image with the specified id.
+#
+# @param id The ID of the image to fetch the information for.
+# @return A reference to the image data on success, undef on error.
+sub get_image_info {
+    my $self = shift;
+    my $id   = shift;
+
+    $self -> clear_error();
+
+    my $imgh = $self -> {"dbh"} -> prepare("SELECT *
+                                            FROM `".$self -> {"settings"} -> {"database"} -> {"images"}."`
+                                            WHERE `id` = ?")
+    $imgh -> execute($id)
+        or return $self -> self_error("Unable to execute image lookup: ".$self -> {"dbh"} -> errstr);
+
+    return $imgh -> fetchrow_hashref();
+}
+
+
+## @method $ store_image($srcfile, $userid)
+# Given a source filename and a userid, move the file into the image filestore
+# (if needed) and then return the information needed to attach to an article.
+#
+# @param srcfile The absolute path to the source file to obtain a path for.
+# @param userid  The ID of the user saving the file
+# @return A reference to the image storage data hash on success, undef on error.
+sub store_image {
+    my $self    = shift;
+    my $srcfile = shift;
+    my $userid  = shift;
+    my $digest;
+
+    $self -> clear_error();
+
+    # Simple grab of the source name
+    my ($srcname) = $srcfile =~ m|([^/]+)$|;
+
+    # Now, calculate the md5 of the file so that duplicate checks can be performed
+    open(IMG, $srcfile)
+        or return $self -> self_error("Unable to open uploaded file '$srcname': $!")
+    binmode(IMG); # probably redundant, but hey
+
+    eval {
+        $digest = Digest -> new("MD5");
+        $digest -> addfile(IMG);
+
+        close(IMG);
+    };
+    return $self -> self_error("An error occurred while processing '$srcname': $@")
+        if($@);
+
+    my $md5 = $digest -> hexdigest;
+
+    # Determine whether a file already exists with the current md5. IF it does,
+    # return the information for the existing file rather than making a new copy.
+    my $exists = $self -> file_md5_lookup($md5);
+    if($exists || $self -> errstr()) {
+        # Log the duplicate hit if appropriate.
+        $self -> {"logger"} -> log($type, $userid, undef, "Request to store image $srcname, already exists as image ".$exists -> {"id"})
+            if($exists);
+
+        return $exists;
+    }
+
+    # File does not appear to be a duplicate, so moving it into the tree should be okay.
+    # The first stage of this is to obtain a new file record ID to use as a unique
+    # directory name.
+    my $newid = $self -> add_file($srcname, $md5)
+        or return undef;
+
+    # Convert the id to a destination directory
+    if(my $outdir = $self -> build_destdir($newid)) {
+        # Now build the paths needed for moving things
+        my $outname = path_join($outdir, $srcname);
+        my $outpath = path_join($self -> {"settings"} -> {"config"} -> {"upload_image_path"}, $outname);
+
+        if(copy($srcfile, $outpath)) {
+            if($self -> update_location($newid, $outname)) {
+                $self -> {"logger"} -> log($type, $userid, undef, "Stored image $srcname in $outname, image id $newid");
+
+                return $self -> get_image_info($newid);
+            }
+        } else {
+            $self -> self_error("Unable to copy image file $srcname: $!");
+        }
+    }
+
+    # Get here and something broke, save the error and clean up before returning it
+    my $errstr = $self -> errstr();
+    $self -> {"logger"} -> log($type, $userid, undef, "Unable to store image $srcname: $errstr");
+
+    $self -> delete_image($newid);
+    return $self -> self_error($errstr);
+}
+
+
+
+# ==============================================================================
+# Private methods
+
+
+## @method private $ build_destdir($id)
+# Given a file id, determine which directory the corresponding file should be stored
+# in, and ensure that the directory tree is in place for it. Note that this will
+# create a hierarchy of directories, up to 100 directories (00 to 99) at the top
+# level, and with up to 100 directories (again, 0 to 99) in each of the top-level
+# directories. This is to reduce the number of files and directories present in
+# any single directory to help out filesystems that struggle with lots of either.
+#
+# @param id The ID of the file to store.
+# @return A path to store the file in, relative to upload_image_path, on success.
+#         undef on error.
+sub build_destdir {
+    my $self = shift;
+    my $id   = shift;
+
+    $self -> clear_error();
+
+    # Pad the id with zeros out to at least 4 characters
+    my $pad    = 4 - length($id);
+    my $padded = $pad > 0 ? ("0" x $pad).$id : $id;
+
+    # Now pull out the bits, and rejoin them into the required form
+    my ($base, $sub) = $padded =~ /^(\d\d)(\d\d)/;
+    my $destdir = path_join($base, $sub, $id);
+
+    # Make sure the path exists
+    my $fullpath = path_join($self -> {"settings"} -> {"config"} -> {"upload_image_path"}, $destdir);
+    eval { make_path($fullpath); };
+    return $self -> self_error("Unable to create image store directory: $@")
+        if($@);
+
+    return $destdir;
+}
+
+
+## @method private $ file_md5_lookup($md5)
+# Look up a file based on the provided md5. Why use MD5 rather than a more secure hash
+# like SHA-256? Primarily as a result of speed (md5 is usually 30% faster), but also
+# because getting duplicate files is really not the end of the world, this is here
+# as a simple check to prevent egregious duplication of uploads by end users, rather
+# than as a vital bastion against the many-angled ones that live at the bottom of the
+# Mandelbrot set.
+#
+# @param md5 The hex-encoded MD5 digest to search for
+# @return A reference to an image record hash on success, undef if the md5 does
+#         not exist, or on error.
+sub file_md5_lookup {
+    my $self = shift;
+    my $md5  = shift;
+
+    $self -> clear_error();
+
+    # Does the md5 match an already present image?
+    my $md5h = $self -> {"dbh"} -> prepare("SELECT *
+                                            FROM `".$self -> {"settings"} -> {"database"} -> {"images"}."`
+                                            WHERE `md5` LIKE ?");
+    $md5h -> execute($md5)
+        or return $self -> self_error("Unable to perform image md5 search: ".$self -> {"dbh"} -> errstr);
+
+    return $md5h -> fetchrow_hashref();
+}
+
+
+## @method private $ add_file($name, $md5)
+# Add an entry for a file to the images table.
+#
+# @param name The name of the image file to add.
+# @param md5  The md5 of the image file being added.
+# @return The id of the new image file row on success, undef on error.
+sub add_file {
+    my $self = shift;
+    my $name = shift;
+    my $md5  = shift;
+
+    $self -> clear_error();
+
+    my $newh = $self -> {"dbh"} -> prepare("INSERT INTO `".$self -> {"settings"} -> {"database"} -> {"images"}."`
+                                            (type, md5, name)
+                                            VALUES('file', ?, ?)");
+    my $rows = $newh -> execute($md5, $name);
+    return $self -> self_error("Unable to perform image file insert: ". $self -> {"dbh"} -> errstr) if(!$rows);
+    return $self -> self_error("Image file insert failed, no rows inserted") if($rows eq "0E0");
+
+    # FIXME: This ties to MySQL, but is more reliable that last_insert_id in general.
+    #        Try to find a decent solution for this mess...
+    # NOTE: the DBD::mysql documentation doesn't actually provide any useful information
+    #       about what this will contain if the insert fails. In fact, DBD::mysql calls
+    #       libmysql's mysql_insert_id(), which returns 0 on error (last insert failed).
+    #       There, why couldn't they bloody /say/ that?!
+    my $newid = $self -> {"dbh"} -> {"mysql_insertid"};
+
+    return $self -> self_error("Unable to obtain id for new image file row")
+        if(!$newid);
+
+    return $newid;
+}
+
+
+## @method private $ delete_image($id)
+# Remove the file entry for the specified row. This is primarily needed to
+# clean up partial file entries that are created during store_image() if that
+# function fails to copy the image into place.
+#
+# @param id The ID of the image row to remove.
+# @return true on success, undef on error.
+sub delete_image {
+    my $self = shift;
+    my $id   = shift;
+
+    $self -> clear_error();
+
+    my $nukeh = $self -> {"dbh"} -> prepare("DELETE FROM `".$self -> {"settings"} -> {"database"} -> {"image"}."`
+                                             WHERE id = ?");
+    $nukeh -> execute($id)
+        or return $self -> self_error("Image delete failed: ".$self -> {"dbh"} -> errstr);
+
+    return 1;
+}
+
+
+## @method private $ update_location($id, $location)
+# Update the image location for the specified image.
+#
+# @param id       The ID of the image to update the location for.
+# @param location The location to set for the image.
+# @return true on success, undef on error.
+sub update_location {
+    my $self     = shift;
+    my $id       = shift;
+    my $location = shift;
+
+    $self -> clear_error();
+
+    my $updateh = $self -> {"dbh"} -> prepare("UPDATE `".$self -> {"settings"} -> {"database"} -> {"images"}."`
+                                               SET `location` = ?
+                                               WHERE `id` = ?");
+    my $result = $atth -> execute($location, $id);
+    return $self -> self_error("Unable to update file location: ".$self -> {"dbh"} -> errstr) if(!$result);
+    return $self -> self_error("File location update failed: no rows updated.") if($result eq "0E0");
+
+    return 1;
 }
 
 1;
