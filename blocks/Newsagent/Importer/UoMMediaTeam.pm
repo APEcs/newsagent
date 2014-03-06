af194a3b6ecb04031f2c713f0e883a792bfe2d35
Chris <chris@starforge.co.uk>
2014-03-06 15:10:06 +0000

    Only include stories with a datePub later than the last update.

--- blocks/Newsagent/Importer/UoMMediaTeam.pm
+++ blocks/Newsagent/Importer/UoMMediaTeam.pm
@@ -21,10 +21,11 @@
 # university of manchester media team. This fetches and processes the
 # XML containing the news stories published by the media team.
 #
-#package Newsagent::Importer::UoMMEdiaTeam;
+package Newsagent::Importer::UoMMEdiaTeam;
 
 use strict;
 #use base qw(Newsagent); # This class extends the Newsagent block class
+use base qw(Webperl::SystemModule);
 use v5.12;
 use DateTime;
 use LWP::UserAgent;
@@ -32,10 +33,63 @@ use XML::Simple;
 use Data::Dumper;
 
 
-
-
 # ============================================================================
-#  Intrnal implementation-specifics
+#  Internal implementation-specifics
+
+## @method private $ _parse_rfc822_datestring($datestr)
+# Parse the specified date string into a new DateTime object.
+#
+# @param datestr The RFC-822 (section 5) date to parse.
+# @return A new DateTime object.
+sub _parse_rfc822_datestring {
+    my $self    = shift;
+    my $datestr = shift;
+    my $monmap  = { "Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4, "May" => 5, "Jun" => 6, "Jul" => 7, "Aug" => 8, "Sep" => 9, "Oct" => 10, "Nov" => 11, "Dec" => 12 };
+
+    $self -> clear_error();
+
+    my ($day, $month, $year, $hour, $minute, $second, $tz) = $datestr =~ /^(?:\w+, )?(\d{2}) (\w+) (\d{4})(?:[T ]?(\d{2}):(\d{2}):(\d{2})(.*))?$/;
+
+    # This should never actually happen - _parse_datestring() should never be able to call
+    # this function if any of these values are bad - but check in case they are zeros.
+    return $self -> self_error("Illegal date format '$datestr'")
+        if(!$day || !$month || !$year)
+
+    # Convert the month
+    $month = $monmap -> {$month};
+    return $self -> self_error("Illegal date format '$datestr': unknown month specified")
+        if(!$month);
+
+    return DateTime -> new(year      => $year,
+                           month     => $month,
+                           day       => $day,
+                           hour      => $hour || 0,
+                           minute    => $minute || 0,
+                           second    => $second || 0,
+                           time_zone => $tz || "UTC");
+}
+
+
+## @method private _parse_datestring($datestr)
+# Identify the format used in the specified date string, and convert it to a
+# DateTime object.
+#
+# @param datestr The date string to parse.
+# @return A DateTime object on success, undef on error.
+sub _parse_datestring {
+    my $self    = shift;
+    my $datestr = shift;
+
+    given($datestr) {
+        when(/^(?:\w+, )?(\d{2}) (\w+) (\d{4})(?:[T ]?(\d{2}):(\d{2}):(\d{2})(.*))?$/) {
+            return $self -> _parse_rfc822_datestring($datestr);
+        }
+        default {
+            return $self -> self_error("Unknown datetime format '$datestr'");
+        }
+    }
+}
+
 
 ## @method private $ _fetch_updated_xml($url, $lastupdate)
 # This will fetch the latest XML file and check whether it needs to be processed.
@@ -44,17 +98,17 @@ use Data::Dumper;
 # @param lastupdate A DateTime object describing the time the xml was last checked.
 # @return A reference to a hash containing updated/new stories.
 sub _fetch_updated_xml {
-#    my $self       = shift;
+    my $self       = shift;
     my $url        = shift;
     my $lastupdate = shift;
 
-#    $self -> clear_error();
+    $self -> clear_error();
 
     my $ua = LWP::UserAgent -> new();
     my $result = $ua -> get($url);
 
     # Bail if the xml file can not be read
-#    return $self -> self_error("Unable to fetch XML file at $url: ".$result -> status_line)
+    return $self -> self_error("Unable to fetch XML file at $url: ".$result -> status_line)
     die "Unable to fetch XML file at $url: ".$result -> status_line."\n"
         unless($result -> is_success);
 
@@ -72,9 +126,15 @@ sub _fetch_updated_xml {
     die "XML parsing $content\nError: $@\n"
         if($@);
 
-    return $tree;
+    my @items = ();
+    foreach my $item (@{$tree -> {"newsitem"}}) {
+        $item -> {"datePub"} = $self -> _parse_datestring($item -> {"datePub"});
+
+        push(@items, $item)
+            if($item -> {"datePub"} > $lastupdate);
+    }
+
+    return \@items;
 }
 
-
-my $tree = _fetch_updated_xml("http://newsadmin.manchester.ac.uk/xml/eps/computerscience/currentmonth.xml", 0);
-print "Tree: ".Dumper($tree)."\n";
+1;
