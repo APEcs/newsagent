f505b6c6807c6310c35e75737251282f91ba7e96
Chris <chris@starforge.co.uk>
2014-03-17 09:40:28 +0000

    Alignment control.

--- templates/common/cke_configs/basic_open.js
+++ templates/common/cke_configs/basic_open.js
@@ -8,8 +8,7 @@ CKEDITOR.editorConfig = function( config )
     config.toolbar_NewsagentBasic = [
 	    { name: 'operations', items : [ 'Source','-','Cut','Copy','Paste','-','Undo','Redo' ] },
 	    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-	    { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
-	                                   '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+	    { name: 'paragraph', items : [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
 	    { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
 	    { name: 'insert', items : [ 'Image', 'HorizontalRule', 'SpecialChar' ] },
 	    { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
