40d42814a243cdfdfac24816f18aac8134b5ca73
Chris <chris@starforge.co.uk>
2014-05-19 13:53:48 +0100

    Support plain text paste.

--- templates/common/cke_configs/basic_open.js
+++ templates/common/cke_configs/basic_open.js
@@ -1,12 +1,14 @@
 CKEDITOR.editorConfig = function( config )
 {
+    config.extraPlugins = 'pastetext';
+
     config.height  = '12em';
     config.toolbarStartupExpanded = true;
     config.toolbar = 'NewsagentBasic';
     config.resize_dir = 'vertical';
 
     config.toolbar_NewsagentBasic = [
-	    { name: 'operations', items : [ 'Source','-','Cut','Copy','Paste','-','Undo','Redo' ] },
+	    { name: 'operations', items : [ 'Source','-','Cut','Copy','Paste','PasteText','-','Undo','Redo' ] },
 	    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
 	    { name: 'paragraph', items : [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
 	    { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
--- templates/common/cke_configs/image_open.js
+++ templates/common/cke_configs/image_open.js
@@ -1,12 +1,14 @@
 CKEDITOR.editorConfig = function( config )
 {
+    config.extraPlugins = 'pastetext';
+
     config.height  = '12em';
     config.toolbarStartupExpanded = true;
     config.toolbar = 'NewsagentBasic';
     config.resize_dir = 'vertical';
 
     config.toolbar_NewsagentBasic = [
-	    { name: 'operations', items : [ 'Source','-','Cut','Copy','Paste','-','Undo','Redo' ] },
+	    { name: 'operations', items : [ 'Source','-','Cut','Copy','Paste','PasteText','-','Undo','Redo' ] },
 	    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
 	    { name: 'paragraph', items : [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl' ] },
 	    { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
@@ -14,4 +16,5 @@ CKEDITOR.editorConfig = function( config )
 	    { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
 	    { name: 'colors', items : [ 'TextColor','BGColor' ] }
     ];
+
 };
