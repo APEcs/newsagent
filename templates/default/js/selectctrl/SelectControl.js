0ba252680740936be10277800a1e213f536977c9
Chris <chris@starforge.co.uk>
2014-06-25 16:18:11 +0100

    Automatic updating of current mode display in place.

--- templates/default/js/selectctrl/SelectControl.js
+++ templates/default/js/selectctrl/SelectControl.js
@@ -12,6 +12,8 @@ var SelectControl = new Class(
     {
         this.setOptions(options);
 
+        this.action = 'open';
+
         // Grab the element, and extend it if needed
         this.element = document.id(element);
         this.element.store('selectctrl_object', this);
@@ -22,14 +24,19 @@ var SelectControl = new Class(
 
     _create: function()
     {
+        // Get the mode icon element
+        this.mode = this.element.getFirst('span.selctrl-mode');
+        if(!this.mode) return;
+
         // fetch the ul which should form the menu
         this.menu = this.element.getFirst('ul');
         if(!this.menu) return;
 
-        this.menu.dispose();                      // remove from the dom
+        this.menu.dispose();                   // remove from the dom
         this.menu.addClass('selctrl-menu');    // ensture the menu has the right class
-        this.menu.setStyle('display', 'none');    // make sure the menu is hidden
+        this.menu.setStyle('display', 'none'); // make sure the menu is hidden
 
+        // Move the menu into position below the control box
         var offset = this.options.offset;
 		var position = this.element.getCoordinates();
 		this.menu.setStyles({'top': position.top + position.height + offset.y,
@@ -37,5 +44,79 @@ var SelectControl = new Class(
 
         this.menu.inject(this.element, 'after'); // and put it back in place
 
+        // Enable menu toggle
+        this.element.addEvents({ 'click': function() {
+                                     if(this.menu.hasClass("open")) {
+                                         this.toggleMenu('close');
+                                     } else {
+                                         this.toggleMenu('open');
+                                     }
+                                 }.bind(this),
+                                 'mouseenter': function() { this.action = 'open'; }.bind(this),
+                                 'mouseleave': function() { this.action = 'close'; }.bind(this),
+                                 'keydown': function(event) {
+                                     if(event.key == 'space' || event.key == 'down' || event.key == 'up') {
+                                         this.action = 'close';
+                                         this.toggleMenu('open');
+                                     }
+                                 }.bind(this)
+                               });
+
+        // clicks elsewhere in the document should close the menu
+        document.addEvents({ 'click': function() {
+                                 if (this.action == 'close') {
+                                     this.toggleMenu('close');
+                                 }
+                             }.bind(this),
+                             'keydown': function(event) {
+                                 if (event.key == 'esc') {
+                                     this.toggleMenu('close');
+                                 }
+                                 if (this.menu.hasClass('open') && (event.key == 'down' || event.key == 'up')) {
+                                     event.stop();
+                                 }
+                             }.bind(this)
+                           });
+
+        // Update the mode display when any checkboxes are ticked
+        $$(this.options.checkClass).each(function(element) {
+            element.addEvent('change', function() { this.updateMode(); }.bind(this));
+        }, this);
+
+        this.updateMode();
+    },
+
+    updateMode: function()
+    {
+        var options = $$(this.options.checkClass);
+        var checked = options.filter(function(box) { return box.get('checked'); });
+
+        if(!options.length) {
+            this.mode.setStyle('background-position', '0px 0px');
+        } else {
+            if(!checked.length) {
+                this.mode.setStyle('background-position', '0px 0px');
+            } else if(checked.length < options.length) {
+                this.mode.setStyle('background-position', '-16px 0px');
+            } else {
+                this.mode.setStyle('background-position', '-32px 0px');
+            }
+        }
+    },
+
+    toggleMenu: function(mode)
+    {
+        if(mode == "close") {
+            this.menu.removeClass("open");
+            this.menu.setStyle('display', 'none');
+
+            this.fireEvent('listOpen', this.element);
+        } else {
+            this.menu.addClass("open");
+            this.menu.setStyle('display', 'block');
+
+            this.fireEvent('listClose', this.element);
+        }
     }
+
 });
