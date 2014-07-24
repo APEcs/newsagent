11c9aaffe2c33a65d2b5df8b2651beeee99b6bd7
Chris <chris@starforge.co.uk>
2014-07-24 13:57:15 +0100

    Handle the situation where there are no target queues available.

--- templates/default/js/messagecontrol/MessageControl.js
+++ templates/default/js/messagecontrol/MessageControl.js
@@ -26,62 +26,62 @@ var MessageControl = new Class(
     _create: function()
     {
         this.move = this.element.getFirst('li.msgctrl-move');
-        if(!this.move) return;
+        if(this.move) {
+            this.menu = this.move.getFirst('ul');
+            if(!this.menu) return;
 
-        this.menu = this.move.getFirst('ul');
-        if(!this.menu) return;
+            this.menu.dispose();                   // remove from the dom
+            this.menu.addClass('msgctrl-menu');    // ensture the menu has the right class
+            this.menu.setStyle('display', 'none'); // make sure the menu is hidden
 
-        this.menu.dispose();                   // remove from the dom
-        this.menu.addClass('msgctrl-menu');    // ensture the menu has the right class
-        this.menu.setStyle('display', 'none'); // make sure the menu is hidden
+            // Move the menu into position below the control box
+            this.positionMenu();
+            this.menu.inject(this.element, 'after'); // and put it back in place
 
-        // Move the menu into position below the control box
-        this.positionMenu();
-        this.menu.inject(this.element, 'after'); // and put it back in place
-
-        // Enable menu toggle
-        this.move.addEvents({ 'click': function() {
-                                  if(this.menu.hasClass("open")) {
-                                      this.toggleMenu('close');
-                                  } else {
+            // Enable menu toggle
+            this.move.addEvents({ 'click': function() {
+                                      if(this.menu.hasClass("open")) {
+                                          this.toggleMenu('close');
+                                      } else {
+                                          this.toggleMenu('open');
+                                      }
+                                  }.bind(this),
+                                  'mouseenter': function() { this.action = 'open'; }.bind(this),
+                                  'mouseleave': function() { this.action = 'close'; }.bind(this),
+                                  'keydown': function(event) {
+                                      if(event.key == 'space' || event.key == 'down' || event.key == 'up') {
+                                          this.action = 'close';
                                       this.toggleMenu('open');
-                                  }
-                              }.bind(this),
-                              'mouseenter': function() { this.action = 'open'; }.bind(this),
-                              'mouseleave': function() { this.action = 'close'; }.bind(this),
-                              'keydown': function(event) {
-                                  if(event.key == 'space' || event.key == 'down' || event.key == 'up') {
-                                      this.action = 'close';
-                                      this.toggleMenu('open');
-                                  }
-                              }.bind(this)
-                            });
+                                      }
+                                  }.bind(this)
+                                });
 
-        // Set up menu options
-        this.menu.getChildren('li').each(function(element) {
-            element.addEvents({ click: function(event) { this.fireEvent('moveMsg', [ event.target.get('data-msgctrl-queue'), this.selectedMsgs() ]);
-                                                         this.toggleMenu('close');
-                                                       }.bind(this),
-                                'mouseenter': function() { this.action = 'open'; }.bind(this),
-                                'mouseleave': function() { this.action = 'close'; }.bind(this)
-                              });
-        }, this);
+            // Set up menu options
+            this.menu.getChildren('li').each(function(element) {
+                                                 element.addEvents({ click: function(event) { this.fireEvent('moveMsg', [ event.target.get('data-msgctrl-queue'), this.selectedMsgs() ]);
+                                                                                              this.toggleMenu('close');
+                                                                                            }.bind(this),
+                                                                     'mouseenter': function() { this.action = 'open'; }.bind(this),
+                                                                     'mouseleave': function() { this.action = 'close'; }.bind(this)
+                                                                   });
+                                             }, this);
 
-        // clicks elsewhere in the document should close the menu
-        document.addEvents({ 'click': function() {
-                                 if (this.action == 'close') {
-                                     this.toggleMenu('close');
-                                 }
-                             }.bind(this),
-                             'keydown': function(event) {
-                                 if (event.key == 'esc') {
-                                     this.toggleMenu('close');
-                                 }
-                                 if (this.menu.hasClass('open') && (event.key == 'down' || event.key == 'up')) {
-                                     event.stop();
-                                 }
-                             }.bind(this)
-                           });
+            // clicks elsewhere in the document should close the menu
+            document.addEvents({ 'click': function() {
+                                     if (this.action == 'close') {
+                                         this.toggleMenu('close');
+                                     }
+                                 }.bind(this),
+                                 'keydown': function(event) {
+                                     if (event.key == 'esc') {
+                                         this.toggleMenu('close');
+                                     }
+                                     if (this.menu.hasClass('open') && (event.key == 'down' || event.key == 'up')) {
+                                         event.stop();
+                                     }
+                                 }.bind(this)
+                               });
+        }
 
         // Update the visibility when any checkboxes are changed
         $$(this.options.checkClass).each(function(element) {
