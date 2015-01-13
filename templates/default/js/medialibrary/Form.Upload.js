2f3fc4aaa03f4c576581435d260f0f2037b1102c
Chris <chris@starforge.co.uk>
2015-01-13 11:09:56 +0000

    Support progress display.

--- templates/default/js/medialibrary/Form.Upload.js
+++ templates/default/js/medialibrary/Form.Upload.js
@@ -39,40 +39,52 @@ Form.Uploader = new Class({
 
     options: {
         url: "",
-        param: 'upload'/*,
+        param: 'upload',
+        uploadmsg: 'Uploading file: {loaded} of {total} ({percent}% complete)',
+        donemsg: 'Complete'/*,
         onSuccess:,
         onFailure:,
         */
     },
 
-    initialize: function(dropArea, progress, options) {
+    initialize: function(dropArea, progress, progressmsg, options) {
         this.hasFormData = ('FormData' in window);
         this.progress = $(progress);
+        this.progmess = $(progressmsg);
         this.dropArea = $(dropArea);
 
         this.setOptions(options);
 
         this.request = new Request.File({ url: this.options.url,
-                                          onRequest: function() { this.progress.setStyles({ display: 'block', width: 0}); },
+                                          onRequest: function() { this.progress.setStyles({ display: 'block', width: 0});
+                                                                  this.progmess.set('html', this.options.uploadmsg.substitute({'loaded': 0,
+                                                                                                                               'total': 0,
+                                                                                                                               'percent': 0}));
+                                                                }.bind(this),
                                           onProgress: function(event){
                                               var loaded = event.loaded, total = event.total;
-                                              this.progress.setStyle('width', parseInt(loaded / total * 100, 10).limit(0, 100) + '%');
-                                          },
+                                              var percent = parseInt(loaded / total * 100, 10).limit(0, 100);
+                                              this.progress.setStyle('width', percent + '%');
+                                              this.progmess.set('html', this.options.uploadmsg.substitute({'loaded': loaded,
+                                                                                                           'total': total,
+                                                                                                           'percent': percent}));
+                                          }.bind(this),
                                           onComplete: function(){
-                                              this.progress.setStyle('width', '100%');
-                                              this.reset();
-                                          },
+                                              this.progress.setStyle('width', '0');
+                                              this.request.reset();
+                                              this.progmess.set('html', this.options.donemsg);
+                                          }.bind(this),
                                           onSuccess: function(responseText, responseXML) {
                                               this.fireEvent('success', responseText, responseXML);
-                                          },
+                                          }.bind(this),
                                           onFailure: function(xhr) {
                                               this.fireEvent('failure', xhr);
-                                          }
+                                          }.bind(this)
                                         });
 
-        this.dropArea.addEvent("dragenter", function() { this.fireEvent('dragenter'); }.bind(this));
-        this.dropArea.addEvent("dragleave", function() { this.fireEvent('dragleave'); }.bind(this));
-        this.dropArea.addEvent("dragend",   function() { this.fireEvent('dragend'); }.bind(this));
+        this.dropArea.addEvent("dragenter", function() { this.fireEvent('dragenter'); console.log('enter'); }.bind(this));
+        this.dropArea.addEvent("dragleave", function() { this.fireEvent('dragleave'); console.log('leave'); }.bind(this));
+        this.dropArea.addEvent("dragend",   function() { this.fireEvent('dragend'); console.log('end'); }.bind(this));
         this.dropArea.addEvent("dragover",  function(event) {
                                    event.preventDefault();
                                    this.fireEvent('dragover', event);
