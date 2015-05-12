ec8ae1e4cb2047fdd94bdaf5b75f2225ef1c8319
Chris <chris@starforge.co.uk>
2015-05-12 13:55:55 +0100

    Fix for new webperl.

--- supportfiles/SendMessages.pl
+++ supportfiles/SendMessages.pl
@@ -8,22 +8,22 @@ use utf8;
 # System modules
 use DBI;
 use Modules;
-use ConfigMicro;
-use Logger;
-use Message::Queue;
+use Webperl::ConfigMicro;
+use Webperl::Logger;
+use Webperl::Message::Queue;
 
-my $logger = Logger -> new()
+my $logger = Webperl::Logger -> new()
     or die "FATAL: Unable to create logger object";
 
 # Load the system config
-my $settings = ConfigMicro -> new("../config/site.cfg")
+my $settings = Webperl::ConfigMicro -> new("../config/site.cfg")
     or $logger -> die_log("Not avilable", "SendMessages.pl: Unable to obtain configuration file: ".$ConfigMicro::errstr);
 
     # Database initialisation. Errors in this will kill program.
 my $dbh = DBI->connect($settings -> {"database"} -> {"database"},
-                                    $settings -> {"database"} -> {"username"},
-                                    $settings -> {"database"} -> {"password"},
-                                    { RaiseError => 0, AutoCommit => 1, mysql_enable_utf8 => 1 })
+                       $settings -> {"database"} -> {"username"},
+                       $settings -> {"database"} -> {"password"},
+                       { RaiseError => 0, AutoCommit => 1, mysql_enable_utf8 => 1 })
     or $logger -> die_log("None", "SendMessages.pl: Unable to connect to database: ".$DBI::errstr);
 
 # Pull configuration data out of the database into the settings hash
@@ -36,14 +36,14 @@ $logger -> init_database_log($dbh, $settings -> {"database"} -> {"logging"})
 # Start doing logging if needed
 $logger -> start_log($settings -> {"config"} -> {"logfile"}) if($settings -> {"config"} -> {"logfile"});
 
-my $messages = Message::Queue -> new(logger   => $logger,
-                                     dbh      => $dbh,
-                                     settings => $settings)
+my $messages = Webperl::Message::Queue -> new(logger   => $logger,
+                                              dbh      => $dbh,
+                                              settings => $settings)
     or $logger -> die_log("none", "SendMessages.pl: Unable to create message handler: ".$SystemModule::errstr);
 
-my $module = Modules -> new(logger   => $logger,
-                            dbh      => $dbh,
-                            settings => $settings)
+my $module = Webperl::Modules -> new(logger   => $logger,
+                                     dbh      => $dbh,
+                                     settings => $settings)
     or $logger -> die_log("none", "SendMessages.pl: Unable to create module loader: ".$SystemModule::errstr);
 
 $messages -> set_module_obj($module);
