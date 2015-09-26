67e41b5cb071e531abc15fb99d520e214e6174f1
Chris <chris@starforge.co.uk>
2015-09-26 14:29:34 +0100

    Progress.

--- modules/Newsagent/Importer/Seminars.pm
+++ modules/Newsagent/Importer/Seminars.pm
@@ -26,7 +26,7 @@ use experimental 'smartmatch';
 use base qw(Newsagent::Importer); # This class extends the Newsagent block class
 use v5.12;
 use DateTime;
-use LWP::UserAgent;
+use DateTime::Format::Strptime;
 use XML::LibXML;
 
 # ============================================================================
@@ -50,9 +50,142 @@ sub import_articles {
 
     $self -> clear_error();
 
+    my $updates = $self -> _fetch_updated_xml($self -> {"args"} -> {"url"}, DateTime -> from_epoch(epoch => $self -> {"importer_lastrun"} || 0))
+        or return undef;
 
 
     return 1;
 }
+
+
+
+# ============================================================================
+#  Internal implementation-specifics
+
+## @fn private $ _sortfn_by_date(void)
+# A function used by sort() to order seminar elements based on the date and
+# time of the seminar. This allows seminars to be sorted into reverse
+# chronological order (future seminars first) using the timestamp set in
+# the element. Note this this function *will not work* unless the list has
+# been processed through _build_datestamps() first - the timestamp element
+# this looks for does not exist in the 'standard' seminar element! If
+# two seminars are set for the same day and time, this will sort them by
+# their IDs.
+#
+# @param a (implicit scalar) A reference to an XML::LibXML::Element for a seminar.
+# @param b (implicit scalar) A reference to an XML::LibXML::Element for a seminar.
+# @return An integer indicating the relation between a and b.
+sub _sortfn_by_date {
+    my ($adate) = $a -> findnodes("timestamp");
+    my ($bdate) = $b -> findnodes("timestamp");
+    my ($aid)   = $a -> findnodes("id");
+    my ($bid)   = $b -> findnodes("id");
+
+    # If the dates match, use IDs to distinguish
+    if($adate -> textContent == $bdate -> textContent) {
+        return $bid -> textContent <=> $aid -> textContent;
+    } else {
+        return $bdate -> textContent <=> $adate -> textContent;
+    }
+}
+
+
+## @method private $ _fetch_updated_xml($url, $lastupdate)
+# This will fetch the latest XML file and check whether it needs to be processed.
+#
+# @param url        The location of the XML file to fetch and process.
+# @param lastupdate A DateTime object describing the time the xml was last checked.
+# @return A reference to a an array of new or updated seminars.
+sub _fetch_updated_xml {
+    my $self       = shift;
+    my $url        = shift;
+    my $lastupdate = shift;
+
+    $self -> clear_error();
+
+    my $dom = eval { XML::LibXML -> load_xml(location => $url); };
+    return $self -> self_error("Unable to process seminar master XML: $@")
+        if($@);
+
+    # Process the seminars so that we only have the ones that haven't happened yet
+    $self -> _build_datestamps($dom) or return undef;
+    my $pending = $self -> _build_pending($dom) or return undef;
+
+    # Now check whether the ones that haven't happened yet need to be added or updated
+
+}
+
+
+## @method private $ _build_datestamps($dom)
+# Ensure that all the seminars in the specified DOM have a unix timestamp
+# set for them in addition to the date and time provided by default.
+#
+# @param dom A reference to an XML::LibXML::Document containing the seminar DOM.
+# @return true on success, undef on error
+sub _build_datestamps {
+    my $self = shift;
+    my $dom  = shift;
+
+    $self -> clear_error();
+
+    # In theory, this should make it easier to fix parsing if the seminar xml changes...
+    my $parser = DateTime::Format::Strptime -> new(pattern   => '%d-%B-%y %H:%M',
+                                                   locale    => "en_GB",
+                                                   time_zone => 'Europe/London',
+                                                   on_error  => 'croak');
+
+    # Go through each seminar adding a unix timestamp
+    foreach my $seminar ($dom -> findnodes("seminars/seminar")) {
+        my ($date) = $seminar -> findnodes("date");
+        my ($time) = $seminar -> findnodes("time");
+        my ($id)   = $seminar -> findnodes("id");
+
+        # The time field is not entirely consistent, this should help to get it
+        # into a HH:MM format we can work with.
+        $time = $time -> textContent;
+        $time =~ s/^(\d\d)pm$/$1:00/;
+        $time =~ s/^(\d\d)(\d\d)$/$1:$2/;
+        $time =~ s/^(\d\d)\.(\d\d)$/$1:$2/;
+
+        # Try the parse; it may not work if the date or time are incorrect formats
+        my $datetime = eval { $parser -> parse_datetime($date -> textContent." ".$time); };
+        return $self -> self_error("Unable to create timestamp for seminar ".$id -> textContent.": $@")
+            if($@);
+
+        # Store the timestamp as seconds since the epoch it UTC.
+        $seminar -> appendTextChild('timestamp', $datetime -> epoch());
+    }
+
+    return 1;
+}
+
+
+## @method private $ _build_pending($dom)
+# Generate a list of seminars FROM THE FUTURE. This looks a the list of seminars
+# in the specified DOM and pulls out the ones that have not yet happened - seminars
+# that have already happened are ignored.
+#
+# @param dom A reference to an XML::LibXML::Document containing the seminar DOM.
+# @return A reference to an array of XML::LibXML::Elements, one per seminar. This
+#         list may be empty if there are no pending seminars.
+sub _build_pending {
+    my $dom = shift;
+    my $now = time();
+
+    my @pending = ();
+    foreach my $item (sort _sortfn_by_date $dom -> findnodes("seminars/seminar")) {
+        my ($stamp) = $item -> findnodes("timestamp");
+
+        # Stop if the item's timestamp is before the current time (seminar has already
+        # happened, so there's no point in maintaining anything for it)
+        last if($stamp -> textContent < $now);
+
+        # Add the XML::LibXML::Element to the pending list.
+        push(@pending, $item);
+    }
+
+    return \@pending;
+}
+
 
 1;
