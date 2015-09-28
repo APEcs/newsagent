626f878398e1d4b1b0ad31f232d6eebe70a92e3f
Chris <chris@starforge.co.uk>
2015-09-28 14:23:42 +0100

    Fixes for autosave and login checks
    Attempts to address autosave issues reported by @tobyhoward - if the compose page is left open on a laptop, and the laptop goes into hibernate mode, the normal heartbeat from autosave will not happen and the session will have expired when the laptop is woken up. Previously, session validity was only checked when creating/editing - it had assumed the autosave heartbeat would keep the session alive. This change enables session checking in autosave features as well.

--- templates/default/js/edittools.js
+++ templates/default/js/edittools.js
@@ -77,220 +77,242 @@ var EditTools = new Class({
      * @return A warning message to show in the beforeunload dialog, or
      *         undef if a warning message is not needed.
      */
-     warn_unload: function() {
-         var warnmsg;
+    warn_unload: function() {
+        var warnmsg;
 
-         this.options.fields.each(function(fieldid) {
-             if(this.get_ckeditor_data(fieldid) !== $(fieldid).retrieve('initialValue')) {
-                 warnmsg = confirm_messages['editwarn'];
-             }
-         }.bind(this));
+        this.options.fields.each(function(fieldid) {
+            if(this.get_ckeditor_data(fieldid) !== $(fieldid).retrieve('initialValue')) {
+                warnmsg = confirm_messages['editwarn'];
+            }
+        }.bind(this));
 
-         this.options.savedHook = window.onbeforeunload;
-         window.onbeforeunload = null;
-         if(warnmsg !== undefined) {
-             setTimeout(function() { window.onbeforeunload = this.options.savedHook; }.bind(this), 1);
-             return warnmsg;
-         }
+        this.options.savedHook = window.onbeforeunload;
+        window.onbeforeunload = null;
+        if(warnmsg !== undefined) {
+            setTimeout(function() { window.onbeforeunload = this.options.savedHook; }.bind(this), 1);
+            return warnmsg;
+        }
 
-         return null;
-     },
+        return null;
+    },
 
-     /** Initialise the autosave stuff. If the edit boxes are empty, attempt to restore
-      *  any previously set autosave. If the edit boxes contain stuff, simply check
-      *  whether any autosave is available.
-      */
-     init_autosave: function() {
-         var allEmpty = true;
+    /** Initialise the autosave stuff. If the edit boxes are empty, attempt to restore
+     *  any previously set autosave. If the edit boxes contain stuff, simply check
+     *  whether any autosave is available.
+     */
+    init_autosave: function() {
+        var allEmpty = true;
 
-         this.options.fields.each(function(fieldid) {
-             if(this.get_ckeditor_data(fieldid) !== '') {
-                 allEmpty = false;
-             }
-         }.bind(this));
+        this.options.fields.each(function(fieldid) {
+            if(this.get_ckeditor_data(fieldid) !== '') {
+                allEmpty = false;
+            }
+        }.bind(this));
 
-         if(allEmpty) {
-             this.load_autosave();
-         } else {
-             this.autosave_store_fields();
-             this.check_autosave();
-         }
+        if(allEmpty) {
+            this.load_autosave();
+        } else {
+            this.autosave_store_fields();
+            this.check_autosave();
+        }
 
-         // turn on the load and save buttons
-         $('autoload').addEvent('click', function() {
-             this.load_autosave();
-         }.bind(this));
+        // turn on the load and save buttons
+        $('autoload').addEvent('click', function() {
+            $('autostatus').set('html', confirm_messages['logcheck']);
+            this.clear_timeout();
 
-         $('autosave').addEvent('click', function() {
-             this.save_autosave(true);
-         }.bind(this));
+            check_login(this.load_autosave.bind(this));
+        }.bind(this));
 
-         // and start the initial save timer
-         this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
-     },
+        $('autosave').addEvent('click', function() {
+            $('autostatus').set('html', confirm_messages['logcheck']);
+            this.clear_timeout();
+
+            check_login(this.save_autosave.bind(this, true));
+        }.bind(this));
+
+        // and start the initial save timer
+        this.timeout_id = window.setTimeout(function() {
+            $('autostatus').set('html', confirm_messages['logcheck']);
+            this.clear_timeout();
+
+            check_login(this.save_autosave.bind(this));
+        }.bind(this), this.options.delay);
+    },
 
 
-     /** Record the current state of the input boxes so that the autosave routine can tell
-      *  if they have changed since the last save.
-      */
-     autosave_store_fields: function() {
+    /** Record the current state of the input boxes so that the autosave routine can tell
+     *  if they have changed since the last save.
+     */
+    autosave_store_fields: function() {
 
         this.options.fields.each(function(fieldid) {
             $(fieldid).store('lastSaved', this.get_ckeditor_data(fieldid));
         }.bind(this));
 
-     },
+    },
 
-     /** Determine whether the user has an autosave set, and if so when it was made.
-      *
-      */
-     load_autosave: function() {
-         // nuke any running timeout if there is one to prevent it saving before the load
-         if(typeof this.timeout_id == "number") {
-             window.clearTimeout(this.timeout_id);
-             delete this.timeout_id;
-         }
+    /** Determine whether the user has an autosave set, and if so when it was made.
+     *
+     */
+    load_autosave: function() {
+        // nuke any running timeout if there is one to prevent it saving before the load
+        this.clear_timeout();
 
-         var req = new Request({ url: api_request_path("webapi", "auto.load", basepath),
-                                 onRequest: function() {
-                                     $('autostate').fade('in');
-                                     $('autostatus').set('html', confirm_messages['restoring']);
-                                 },
-                                 onSuccess: function(respText, respXML) {
-                                     $('autostate').fade('out');
+        var req = new Request({ url: api_request_path("webapi", "auto.load", basepath),
+                                onRequest: function() {
+                                    $('autostate').fade('in');
+                                    $('autostatus').set('html', confirm_messages['restoring']);
+                                },
+                                onSuccess: function(respText, respXML) {
+                                    $('autostate').fade('out');
 
-                                     var err = respXML.getElementsByTagName("error")[0];
-                                     if(err) {
-                                         $('autostatus').set('html', err.getAttribute('info'));
-                                     } else {
-                                         var response = respXML.getElementsByTagName("result")[0];
+                                    var err = respXML.getElementsByTagName("error")[0];
+                                    if(err) {
+                                        $('autostatus').set('html', err.getAttribute('info'));
+                                    } else {
+                                        var response = respXML.getElementsByTagName("result")[0];
 
-                                         // Is any autosave data available?
-                                         if(response.getAttribute('autosave') == "available") {
-                                             this.options.fields.each(function(fieldid) {
-                                                 var fielddata = respXML.getElementById(fieldid);
-                                                 if(fielddata) {
-                                                     this.set_ckeditor_data(fieldid, fielddata.textContent);
-                                                 }
-                                             }.bind(this));
+                                        // Is any autosave data available?
+                                        if(response.getAttribute('autosave') == "available") {
+                                            this.options.fields.each(function(fieldid) {
+                                                var fielddata = respXML.getElementById(fieldid);
+                                                if(fielddata) {
+                                                    this.set_ckeditor_data(fieldid, fielddata.textContent);
+                                                }
+                                            }.bind(this));
 
-                                             $('autoloadopt').fade('in');
-                                         }
+                                            $('autoloadopt').fade('in');
+                                        }
 
-                                         $('autostatus').set('html', response.getAttribute('desc'));
+                                        $('autostatus').set('html', response.getAttribute('desc'));
 
-                                         // Record the new state of the boxes
-                                         this.autosave_store_fields();
+                                        // Record the new state of the boxes
+                                        this.autosave_store_fields();
 
-                                         // restart the timer
-                                         this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
-                                     }
-                                 }.bind(this)
-                               });
-         req.post();
-     },
+                                        // restart the timer
+                                        this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
+                                    }
+                                }.bind(this),
+                                onFailure: function(xhr) {
+                                    $('autostate').fade('out');
+                                    $('autostatus').set('html', confirm_messages['failed'] + ":" + xhr.statusText);
+                                }
+                              });
+        req.post();
+    },
 
-     /** Determine whether the user has an autosave, and update the UI but do
-      *  not restore any content.
-      */
-     check_autosave: function() {
-         var req = new Request({ url: api_request_path("webapi", "auto.check", basepath),
-                                 onRequest: function() {
-                                     $('autostate').fade('in');
-                                     $('autostatus').set('html', confirm_messages['checking']);
-                                 },
-                                 onSuccess: function(respText, respXML) {
-                                     $('autostate').fade('out');
+    /** Determine whether the user has an autosave, and update the UI but do
+     *  not restore any content.
+     */
+    check_autosave: function() {
+        var req = new Request({ url: api_request_path("webapi", "auto.check", basepath),
+                                onRequest: function() {
+                                    $('autostate').fade('in');
+                                    $('autostatus').set('html', confirm_messages['checking']);
+                                },
+                                onSuccess: function(respText, respXML) {
+                                    $('autostate').fade('out');
 
-                                     var err = respXML.getElementsByTagName("error")[0];
-                                     if(err) {
-                                         $('autostatus').set('html', err.getAttribute('info'));
-                                     } else {
-                                         var response = respXML.getElementsByTagName("result")[0];
+                                    var err = respXML.getElementsByTagName("error")[0];
+                                    if(err) {
+                                        $('autostatus').set('html', err.getAttribute('info'));
+                                    } else {
+                                        var response = respXML.getElementsByTagName("result")[0];
 
-                                         if(response.getAttribute('autosave') == "available") {
-                                             $('autoloadopt').fade('in');
-                                         }
+                                        if(response.getAttribute('autosave') == "available") {
+                                            $('autoloadopt').fade('in');
+                                        }
 
-                                         $('autostatus').set('html', response.getAttribute('desc'));
-                                     }
-                                 }
-                               });
-         req.post();
-     },
+                                        $('autostatus').set('html', response.getAttribute('desc'));
+                                    }
+                                }
+                              });
+        req.post();
+    },
 
 
-     save_autosave: function(force) {
-         // nuke any running timeout if there is one
-         if(typeof this.timeout_id == "number") {
-             window.clearTimeout(this.timeout_id);
-             delete this.timeout_id;
-         }
+    save_autosave: function(force) {
+        // nuke any running timeout if there is one
+        this.clear_timeout();
 
-         // ignore repeated save attempts
-         if(this.options.saving) {
-             return false;
-         }
-         this.options.saving = true;
+        // ignore repeated save attempts
+        if(this.options.saving) {
+            return false;
+        }
+        this.options.saving = true;
 
-         // Have any of the field changed, or is a save being forced?
-         var changed = force;
-         if(!changed) {
-             this.options.fields.each(function(fieldid) {
-                 if(this.get_ckeditor_data(fieldid) !== $(fieldid).retrieve('lastSaved')) {
-                     changed = true;
-                 }
-             }.bind(this));
-         }
+        // Have any of the field changed, or is a save being forced?
+        var changed = force;
+        if(!changed) {
+            this.options.fields.each(function(fieldid) {
+                if(this.get_ckeditor_data(fieldid) !== $(fieldid).retrieve('lastSaved')) {
+                    changed = true;
+                }
+            }.bind(this));
+        }
 
-         // If nothing is being changed, let others back in and restore the timer
-         if(!changed) {
-             this.options.saving = false;
+        // If nothing is being changed, let others back in and restore the timer
+        if(!changed) {
+            this.options.saving = false;
 
-             // restart the timer
-             this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
+            // restart the timer
+            this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
 
-             return false;
-         }
+            return false;
+        }
 
-         var req = new Request({ url: api_request_path("webapi", "auto.save", basepath),
-                                 onRequest: function() {
-                                     $('autostate').fade('in');
-                                     $('autoloadopt').fade('out');
-                                     $('autostatus').set('html', confirm_messages['saving']);
-                                 },
-                                 onSuccess: function(respText, respXML) {
-                                     $('autostate').fade('out');
+        var req = new Request({ url: api_request_path("webapi", "auto.save", basepath),
+                                onRequest: function() {
+                                    $('autostate').fade('in');
+                                    $('autoloadopt').fade('out');
+                                    $('autostatus').set('html', confirm_messages['saving']);
+                                },
+                                onSuccess: function(respText, respXML) {
+                                    $('autostate').fade('out');
 
-                                     var err = respXML.getElementsByTagName("error")[0];
-                                     if(err) {
-                                         $('autostatus').set('html', err.getAttribute('info'));
-                                     } else {
-                                         var response = respXML.getElementsByTagName("result")[0];
+                                    var err = respXML.getElementsByTagName("error")[0];
+                                    if(err) {
+                                        $('autostatus').set('html', err.getAttribute('info'));
+                                    } else {
+                                        var response = respXML.getElementsByTagName("result")[0];
 
-                                         if(response.getAttribute('autosave') == "available") {
-                                             $('autoloadopt').fade('in');
-                                         }
+                                        if(response.getAttribute('autosave') == "available") {
+                                            $('autoloadopt').fade('in');
+                                        }
 
-                                         $('autostatus').set('html', response.getAttribute('desc'));
-                                     }
-                                     this.options.saving = false;
+                                        $('autostatus').set('html', response.getAttribute('desc'));
+                                    }
+                                    this.options.saving = false;
 
-                                     // Record the new state of the boxes
-                                     this.autosave_store_fields();
+                                    // Record the new state of the boxes
+                                    this.autosave_store_fields();
 
-                                     // restart the timer
-                                     this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
-                                 }.bind(this)
-                               });
+                                    // restart the timer
+                                    this.timeout_id = window.setTimeout(function() { this.save_autosave(); }.bind(this), this.options.delay);
+                                }.bind(this),
+                                onFailure: function(xhr) {
+                                    $('autostate').fade('out');
+                                    $('autostatus').set('html', confirm_messages['failed'] + ":" + xhr.statusText);
+                                }
+                              });
 
-         var data = {};
-         this.options.fields.each(function(fieldid) {
-             data[fieldid] = this.get_ckeditor_data(fieldid);
-         }.bind(this));
+        var data = {};
+        this.options.fields.each(function(fieldid) {
+            data[fieldid] = this.get_ckeditor_data(fieldid);
+        }.bind(this));
 
-         req.post(data);
-     }
+        req.post(data);
+    },
+
+
+    clear_timeout: function() {
+        if(typeof this.timeout_id == "number") {
+            window.clearTimeout(this.timeout_id);
+            delete this.timeout_id;
+        }
+    }
+
 });
 
 
