df9e6d1b3ec6be6336b650e10b1549be5eab7c7f
Chris <chris@starforge.co.uk>
2017-05-25 15:35:14 +0100

    Deprecate the old REST API
    This will need to be replaced over time, but removing it immediately
    would require substantial rewrites of multiple aspects of the system.

--- blocks/Newsagent/Article/API.pm
+++ blocks/Newsagent/Article/API.pm
@@ -3,6 +3,9 @@
 #
 # @author  Chris Page &lt;chris@starforge.co.uk&gt;
 #
+# @deprecated This API is deprecated and should not be used in new code;
+#             the features should be provided in Newsagent::API
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
